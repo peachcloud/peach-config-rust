a63cab054a8e0388b51a0548ced33d2887c62121
mhfowler <maxhfowler@gmail.com>
2021-07-23 17:27:50 -0400

    Add install.sh script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo "deb http://apt.peachcloud.org/ buster main" > /etc/apt/sources.list.d/peach.list
+wget -O - http://apt.peachcloud.org/pubkey.gpg | sudo apt-key add -
+apt-get update
+apt-get install -y peach-config
